625d10cf74b58c53185dead4aaadbdb6ad2897fe
Mike <michael.adda92@gmail.com>
2025-09-17 15:04:18 +0200

    fix : null user case fixed

--- lab/src/controllers/user.js
+++ lab/src/controllers/user.js
@@ -1,34 +1,43 @@
-const db = require('../dbClient')
+const db = require('../dbClient');
 
 module.exports = {
   create: (user, callback) => {
-    // Check parameters
-    if(!user.username)
-      return callback(new Error("Wrong user parameters"), null)
-    // Create User schema
+    // Check if the username parameter is provided
+    if (!user.username) return callback(new Error("Wrong user parameters"), null);
+
+    // Create the user object
     const userObj = {
       firstname: user.firstname,
       lastname: user.lastname,
-    }
-    // Save to DB
-    // TODO check if user already exists
-    db.hmset(user.username, userObj, (err, res) => {
-      if (err) return callback(err, null)
-      callback(null, res) // Return callback
-    })
+    };
+
+    // Check if the user already exists in Redis
+    db.exists(user.username, (err, exists) => {
+      if (err) return callback(err, null);           // Return error if Redis fails
+      if (exists === 1) return callback(new Error("User already exists"), null); // User exists
+
+      // Save the new user to Redis
+      db.hmset(user.username, userObj, (err, res) => {
+        if (err) return callback(err, null);        // Return error if Redis fails
+        callback(null, res);              // Return 'OK' if success
+      });
+    });
   },
-  
+
   get: (username, callback) => {
-    if (!username) return callback(new Error("Username required"), null)
+    // Check if the username is provided
+    if (!username) return callback(new Error("Username required"), null);
 
+    // Check if the user exists in Redis
     db.exists(username, (err, exists) => {
-      if (err) return callback(err, null)
-      if (!exists) return callback(new Error("User not found"), null)
+      if (err) return callback(err, null);         // Return error if Redis fails
+      if (exists !== 1) return callback(new Error("User not found"), null); // User does not exist
 
+      // Retrieve all user fields from Redis
       db.hgetall(username, (err, user) => {
-        if (err) return callback(err, null)
-        callback(null, { username, ...user })
-      })
-    })
+        if (err) return callback(err, null);       // Return error if Redis fails
+        callback(null, { username, ...user });      // Return user object
+      });
+    });
   }
-}
+};
